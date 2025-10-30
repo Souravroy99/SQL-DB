58fee4c3b7f3a6a2e18e7358cc07aa1732e0c708
Souravroy99 <souravroy200418@gmail.com>
2025-10-30 13:51:42 +0530

    ALTER

--- 03_ALTER_OP.cpp
+++ 03_ALTER_OP.cpp
@@ -1,20 +1,34 @@
 // Add New Column
-1.) ALTER TABLE table_name ADD column_name data_type;
-2.) ALTER TABLE table_name ADD column_name data_type NOT NULL DEFAULT 0;
+1.) ALTER TABLE table_name 
+    ADD COLUMN column_name data_type;
+
+2.) ALTER TABLE table_name 
+    ADD COLUMN column_name data_type NOT NULL DEFAULT 0;
 
 
-// Change Column Datatype
-1.) ALTER TABLE table_name MODIFY column_name data_type;
-2.) ALTER TABLE table_name MODIFY column_name data_type NOT NULL DEFAULT 0;
-
-
-// Change Column Name 
-ALTER TABLE table_name  CHANGE  old_column_name  new_column_name  new_column_datatype
 
 
 // DROP Column
-ALTER TABLE table_name DROP COLUMN column_name
+ALTER TABLE table_name 
+DROP COLUMN column_name
 
 
 // Rename Table
 ALTER TABLE table_name  RENAME  TO  new_table_name
+
+
+
+
+/* "RENAME"  VS  "MODIFY"  VS  "CHANGE" */
+ 
+// RENAME:  Change Column Name 
+    ALTER TABLE table_name  
+    RENAME COLUMN  old_column_name  TO  new_column_name 
+
+// MODIFY:  Change type, keep name 
+    ALTER TABLE table_name 
+    MODIFY COLUMN  column_name  new_data_type  NOT NULL;
+
+// CHANGE:  Rename + Change type (CHANGE needs both old and new name + type)
+    ALTER TABLE table_name 
+    CHANGE COLUMN  old_column_name  new_column_name  new_data_type;
