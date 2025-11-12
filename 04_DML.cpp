2596ac0531a93dcb6131206a8008e8d6a728f552
Souravroy99 <souravroy200418@gmail.com>
2025-11-13 01:18:38 +0530

    Formatting in DML

--- 04_DML.cpp
+++ 04_DML.cpp
@@ -1,9 +1,13 @@
 // Update column value
-UPDATE table_name  SET  column_name_1="value_1", ... column_name_n="value_n"  WHERE ID = "VALUE"
+UPDATE table table_name  
+SET  column_name_1="value_1", ... column_name_n="value_n"  
+WHERE ID = "VALUE"
 
 
 // Delete row
-DELETE FROM table_name WHERE id = "VALUE" 
+DELETE FROM table_name 
+WHERE id = "VALUE" 
+
 
 // Delete Whole Table
 DELETE FROM table_name
--- MYSQL Local.session.sql
+++
@@ -1,8 +0,0 @@
-USE temp;
-
-INSERT INTO student VALUES(1, 'Amit');
-INSERT InTO student VALUES(2, 'Souvik');
-
-SELECT * FROM student
-
-
